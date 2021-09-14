99a752c9f4e8081c487d427a4aaa13a04f061fc4
saniatok <89937057+saniatok@users.noreply.github.com>
2021-09-14 15:26:53 +0300

    pipe_gas_2

---
+++ Okoneshnikova_pipe.cpp
@@ -0,0 +1,21 @@
+// Okoneshnikova_pipe.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+struct Pipe
+{
+    int id;
+    int d;
+};
+
+int main()
+{
+    Pipe p;
+    p.id = 0;
+    p.d = 0;
+    std::cout << "Enter diameter of pipe";
+   std::cin >> p.d;
+   std::cout << p.d;
+
+}
+
