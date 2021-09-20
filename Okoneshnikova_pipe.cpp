feaa907130318946e7034483c7c407c645446eb6
saniatok <saniat.ok@mail.ru>
2021-09-20 19:26:21 +0300

    добавление вывода и изменения в трубе

--- Okoneshnikova_pipe.cpp
+++ Okoneshnikova_pipe.cpp
@@ -1,21 +1,58 @@
-// Okoneshnikova_pipe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
 struct Pipe
 {
     int id;
-    int d;
+    int diam;
+    float length;
+    bool in_repair;
+};
+
+Pipe InputPipe()
+{
+    Pipe p;
+    cout << "Enter diameter of pipe: ";
+    cin >> p.diam;
+    cout << "Enter length of pipe: ";
+    cin >> p.length;
+    p.id++;
+    return p;
+    cout << endl;
+}
+
+void OutputPipe(const Pipe& p)
+{
+    cout << "Pipe's id: " << p.id << endl
+        << "Pipe's diameter: " << p.diam << endl
+        << "Pipe's length: " << p.length << endl;
+    if (p.in_repair)
+    {
+        cout << "This pipe isn't in repair. " << endl;
+    }
+    else
+    {
+        cout << "This pipe in repair." << endl;
+    };
+    cout << endl;
+};
+
+void EditPipe(Pipe& p)
+{
+    p.in_repair = !p.in_repair;
 };
 
 int main()
 {
     Pipe p;
     p.id = 0;
-    p.d = 0;
-    std::cout << "Enter diameter of pipe";
-   std::cin >> p.d;
-   std::cout << p.d;
-
+    p.diam = 0;
+    p.length = 0.0;
+    p.in_repair = false;
+    Pipe pl = InputPipe();
+    OutputPipe(pl);
+    EditPipe(pl);
+    OutputPipe(pl);
+    return 0;
 }
 
